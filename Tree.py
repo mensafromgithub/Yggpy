4481ac719ca05bd230d0026ee217ea0f49ff18d1
mensafromgithub <120187974+mensafromgithub@users.noreply.github.com>
2023-05-12 00:01:38 +0300

    Update Tree.py

--- Tree.py
+++ Tree.py
@@ -1,4 +1,5 @@
 from telebot.types import InlineKeyboardMarkup, ReplyKeyboardMarkup
+from telebot import TeleBot
 from TreeExceptions import *
 
 
@@ -36,7 +37,7 @@ class Tree:
             try:
                 return self.twigs[list(self.twigs.keys())[ind]]
             except:
-                return None
+                raise KeyError(f'Undefined index or key: {ind}')
 
     def __str__(self):
         return str(self.twigs)
@@ -49,23 +50,25 @@ class Twig:
         self.bot = bot
         self.user = user
         self.callback_handlers = {}
+        self.custom_candlers = {}
+        self.custom_mendlers = {}
 
-    def __getitem__(self, ind):
+    def __getitem__(self, ind): #
         try:
             return self.signals[ind]
         except:
             try:
                 return self.signals[list(self.signals.keys())[ind]]
             except:
-                return None
+                raise KeyError(f'Undefined index or key: {ind}')
 
-    def __len__(self):
+    def __len__(self): #
         return len(self.signals)
                  
-    def __call__(self, data=None,  ind=0):
+    def __call__(self, data=None,  ind=0): 
         try:
                 sig = self.signals[ind]
-                n = [i for i in range(len(self[ind])) if self[ind][i](data)]
+                n = [i for i in range(len(self[ind])) if self[ind][i].check_condition(data)]
                 if not n:
                     msg = self.bot.send_message(sig[0].chat_id, 'Неверный ввод')
                     self.bot.register_next_step_handler(msg, self, ind=ind)
@@ -76,57 +79,76 @@ class Twig:
                         self.bot.register_next_step_handler(msg, self, ind=ind + 1)
                     elif sig[n].content_type == 'twig':
                         sig[n].params['twig'](ind=0)
+                    elif sig[n].content_type == 'photo':
+                        msg = self.bot.send_photo(*list(sig[n].params.values()))
+                        self.bot.register_next_step_handler(msg, self, ind=ind + 1)
+                    elif sig[n].content_type == 'document':
+                        msg = self.bot.send_document(*list(sig[n].params.values()))
+                        self.bot.register_next_step_handler(msg, self, ind=ind + 1)
         except:
             pass
 
-    def __repr__(self):
+    def __repr__(self): #
         return str(self.signals)
 
-    def __str__(self):
+    def __str__(self): #
         return str(self.signals)
 
-    def set_bot(self, bot):
-        self.bot = bot
+    def set_bot(self, bot): #
+        if isinstance(bot, TeleBot):
+            self.bot = bot
+            for i in range(len(self.callback_handlers)):
+                for j in range(len(self.callback_handlers[i])):
+                    self.callback_handlers[i][j](self.bot)
 
-    def set_user(self, user):
-        if user.isdigi():
+    def set_user(self, user): #
+        if user.isdigit():
             self.user = user
-            for i in self.signals:
-                for j in self.signals[i]:
-                    self.signals[i][j]['chat_id'] = self.user
+            for i in range(len(self.signals) - 1):
+                for j in range(len(self.signals[i])):
+                    if self.signals[i][j].content_type != 'twig':
+                        self.signals[i][j].params['chat_id'] = self.user
             return True
         return False
 
     @staticmethod
-    def switch_conditions(leaf1, leaf2):
+    def switch_conditions(leaf1, leaf2): #
         leaf1.condition, leaf2.condition = leaf2.condition, leaf1.condition
 
     @staticmethod
-    def switch_keyboards(leaf1, leaf2):
+    def switch_keyboards(leaf1, leaf2): #
         leaf1.keyboard, leaf2.keyboard = leaf2.keyboard, leaf1.keyboard
 
     def make_metre(self, cont): # 
-        if (not isinstance(cont, Twig)) and (not isinstance(cont, Leaf)):
+        if not isinstance(cont, Leaf):
                 return False
         n = len(self) if len(self) > 0 else len(self) + 1
         self.signals[n - 1] = {0: cont}
         self.signals[len(self)] = lambda x: x
         self.callback_handlers[n - 1] = {0: {}}
         if cont.keyboard != None:
-            for i, value in enumerate(cont.keyboard.keyboard):
-                self.callback_handlers[n - 1][0][i] = self.bot.callback_query_handler(func=lambda x: x == value[0].callback_data)(lambda: self(data=str(value[0].callback_data), ind=n))
+            if not self.bot:
+                for i, value in enumerate(cont.keyboard.keyboard):
+                    self.callback_handlers[n - 1][i] = lambda bot: bot.callback_query_handler(func=lambda x: x == value[0].callback_data)(lambda: self(data=str(value[0].callback_data), ind=n))
+            else:
+                for i, value in enumerate(cont.keyboard.keyboard):
+                    self.callback_handlers[n - 1][i] = self.bot.callback_query_handler(func=lambda x: x == value[0].callback_data)(lambda: self(data=str(value[0].callback_data), ind=n))
         return True
 
     def replace_metre(self, cont, ind): #
         if ind not in range(len(self)):
-            return False
-        if (not isinstance(cont, Twig)) and (not isinstance(cont, Leaf)):
+            raise KeyError(f'Undefined index or key: {ind}')
+        if not isinstance(cont, Leaf):
                 return False
         self.signals[ind] = {0: cont}
         self.callback_handlers[ind] = {0: {}}
         if cont.keyboard != None:
-            for i, value in enumerate(cont.keyboard.keyboard):
-                self.callback_handlers[n - 1][0][i] = self.bot.callback_query_handler(func=lambda x: x == value[0].callback_data)(lambda: self(data=str(value[0].callback_data), ind=n))
+            if not self.bot:
+                for i, value in enumerate(cont.keyboard.keyboard):
+                    self.callback_handlers[ind][i] = lambda bot: bot.callback_query_handler(func=lambda x: x == value[0].callback_data)(lambda: self(data=str(value[0].callback_data), ind=n))
+            else:
+                for i, value in enumerate(cont.keyboard.keyboard):
+                    self.callback_handlers[ind][i] = self.bot.callback_query_handler(func=lambda x: x == value[0].callback_data)(lambda: self(data=str(value[0].callback_data), ind=n))
         return True
 
     def switch_metre(self, switchable, switched): #
@@ -147,32 +169,28 @@ class Twig:
 
     def del_condition(self, ind): #
         if ind not in range(len(self)):
-            return False
+            raise KeyError(f'Undefined index or key: {ind}')
         if len(self[ind]) != 1:
             return False
         self[ind][0].set_condition(lambda x: True)
         return True
 
-    def set_conditions(self, ind, conditions):
+    def set_conditions(self, ind, conditions): #
         if ind not in range(len(self)):
-            return False
+            raise KeyError(f'Undefined index or key: {ind}')
         if len(conditions) != len(self[ind]):
-            return False
-        if not all([type(i) == type(lambda x: True) for i in conditions]):
             return False
         if any([i('-300') for i in conditions]):
             return False
         for i in range(len(self[ind])):
-            self[ind][i].set_condition(condition[i])
+            self[ind][i].set_condition(conditions[i])
         return True
 
     def set_condition(self, ind, n, condition): #
-        if type(condition) != type(lambda x: True):
-            return False
         if ind not in range(len(self)):
-            return False
+            raise KeyError(f'Undefined index or key: {ind}')
         if n not in range(len(self[ind])):
-            return False
+            raise KeyError(f'Undefined index or key: {ind}/{n}')
         if condition('-300'):
             return False
         self[ind][n].set_condition(condition)
@@ -188,9 +206,9 @@ class Twig:
         return True
 
 
-    def switch_conditions_without_leafs(self, ind, switchable, switched):
+    def switch_conditions_without_leafs(self, ind, switchable, switched): #
         if ind not in range(len(self)):
-            return False
+            raise KeyError(f'Undefined index or key: {ind}')
         if switchable not in range(len(self)) or switched not in range(len(self)):
             return False
         Twig.switch_conditions(self[ind][switchable], self[ind][switched])
@@ -198,19 +216,23 @@ class Twig:
 
     def replace_leaf(self, cont, ind, n): #
         if ind not in range(len(self)):
-            return False
+            raise KeyError(f'Undefined index or key: {ind}')
         if n not in range(len(self[ind])):
-            return False
+            raise KeyError(f'Undefined index or key: {ind}/{n}')
         self.signals[ind][n] = cont
         self.callback_handlers[ind][n] = {}
         if cont.keyboard != None:
-            for i, value in enumerate(cont.keyboard.keyboard):
-                self.callback_handlers[n - 1][n][i] = self.bot.callback_query_handler(func=lambda x: x == value[0].callback_data)(lambda: self(data=str(value[0].callback_data), ind=n))
+            if not self.bot:
+                for i, value in enumerate(cont.keyboard.keyboard):
+                    self.callback_handlers[ind][n] = lambda bot: bot.callback_query_handler(func=lambda x: x == value[0].callback_data)(lambda: self(data=str(value[0].callback_data), ind=n))
+            else:
+                for i, value in enumerate(cont.keyboard.keyboard):
+                    self.callback_handlers[ind][n] = self.bot.callback_query_handler(func=lambda x: x == value[0].callback_data)(lambda: self(data=str(value[0].callback_data), ind=n))
         return True
 
-    def switch_leaf_without_conds(self, ind, switchable, switched):
+    def switch_leaf_without_conds(self, ind, switchable, switched): #
         if ind not in range(len(self)):
-            return False
+            raise KeyError(f'Undefined index or key: {ind}')
         if switchable not in range(len(self)) or switched not in range(len(self)):
             return False
         Twig.switch_conditions(self[ind][switchable], self[ind][switched])
@@ -218,50 +240,48 @@ class Twig:
         return True
     
     def add_leaf(self, cont, ind): #
-            if ind not in range(len(self)):
-             return False
-            if (not isinstance(cont, Twig)) and (not isinstance(cont, Leaf)):
-                return False
-            n = len(self[ind])
-            self.signals[ind][n] = cont
-            self.callback_handlers[ind][n] = {}
-            if cont.keyboard != None:
-                for i, value in enumerate(cont.keyboard.keyboard):
-                    self.callback_handlers[ind][n][i] = self.bot.callback_query_handler(func=lambda x: x == value[0].callback_data)(lambda: self(data=str(value[0].callback_data), ind=n))
-            return True
-
-    def del_metre(self, ind):
         if ind not in range(len(self)):
+            raise KeyError(f'Undefined index or key: {ind}')
+        if not isinstance(cont, Leaf):
             return False
+        n = len(self[ind])
+        self.signals[ind][n] = cont
+        self.callback_handlers[ind][n] = {}
+        if cont.keyboard != None:
+            if not self.bot:
+                for i, value in enumerate(cont.keyboard.keyboard):
+                    self.callback_handlers[ind][n] = lambda bot: bot.callback_query_handler(func=lambda x: x == value[0].callback_data)(lambda: self(data=str(value[0].callback_data), ind=n))
+            else:
+                for i, value in enumerate(cont.keyboard.keyboard):
+                    self.callback_handlers[ind][n] = self.bot.callback_query_handler(func=lambda x: x == value[0].callback_data)(lambda: self(data=str(value[0].callback_data), ind=n))
+        return True
+
+    def del_metre(self, ind): #
+        if ind not in range(len(self)):
+            raise KeyError(f'Undefined index or key: {ind}')
         k = list(self.signals.keys())
         for i in range(ind, len(self) - 1):
             self.signals[k[i]] =  self[k[i + 1]]
         del self.signals[k[-1]]
         return True
 
-    def del_leaf(self, ind, n):
+    def del_leaf(self, ind, n): #
         if ind not in range(len(self)):
-            return False
+            raise KeyError(f'Undefined index or key: {ind}')
         if n not in range(len(self[ind])):
-            return False
+            raise KeyError(f'Undefined index or key: {ind}/{n}')
         k = list(self[ind].keys())
         for i in range(n, len(self[ind]) - 1):
             self[ind][k[i]] =  self[ind][k[i + 1]]
         del self[ind][k[-1]]
         return True
 
-    def get_leaf(self, ind, n):
+    def get_leaf(self, ind, n): #
         if ind not in range(len(self)):
-            return False
+            raise KeyError(f'Undefined index or key: {ind}')
         if n not in range(len(self[ind])):
-            return False
+            raise KeyError(f'Undefined index or key: {ind}/{n}')
         return self[ind][n]
-
-    def set_message_handler(self):
-        pass
-
-    def set_callback_query_handler(self):
-        pass
 
     def get_connection(self, ind=0):
         cons = ''
@@ -271,10 +291,22 @@ class Twig:
         cons += ' ...'
         return cons
 
+    def set_custom_candler(self, candler, name):
+        self.custom_candlers[name] = candler
+
+    def del_custom_candler(self, name):
+        del self.custom_candlers[name]
+
+    def set_custom_mendler(self, candler, name):
+        self.custom_mendlers[name] = candler
+
+    def del_custom_mendler(self, name):
+        del self.custom_mendlers[name]
+
 
 class Leaf:
     def __init__(self, content_type, keyboard=None, condition=lambda x: True):
-        if not ((keyboard != None) == (isinstance(keyboard, InlineKeyboardMarkup) or isinstance(keyboard, ReplyKeyboardMarkup))):
+        if (keyboard != None) != (isinstance(keyboard, InlineKeyboardMarkup) or isinstance(keyboard, ReplyKeyboardMarkup)):
             raise TypeError({'Error': {'Type': type(keyboard)}, 'attribute': {'Name': 'keyboard', 'Types': [type(InlineKeyboardMarkup()), type(ReplyKeyboardMarkup())]}})
         if content_type not in ['text', 'photo', 'document']:
             raise TypeError({'Error': {'Type': type(content_type)}, 'attribute': {'Name': 'condition', 'Types': [type('')]}})
@@ -325,6 +357,43 @@ class Leaf:
                                    'protect_content': None,
                                    'message_thread_id': None,
                                    'thumb': None},
+                      "audio": {'chat_id': None,
+                                'audio': None,
+                                'caption': None,
+                                'duration': None,
+                                'performer': None,
+                                'title': None,
+                                'reply_to_message_id': None,
+                                'reply_markup': None,
+                                'parse_mode': None,
+                                'disable_notification': None,
+                                'timeout': None,
+                                'thumbnail': None,
+                                'caption_entities': None,
+                                'allow_sending_without_reply': None,
+                                'protect_content': None,
+                                'message_thread_id': None,
+                                'thumb': None},
+                      "video": {'chat_id': None,
+                                'video': None,
+                                'duration': None,
+                                'width': None,
+                                'height': None,
+                                'thumbnail': None,
+                                'caption': None,
+                                'parse_mode': None,
+                                'caption_entities': None,
+                                'supports_streaming': None,
+                                'disable_notification': None,
+                                'protect_content': None,
+                                'reply_to_message_id': None,
+                                'allow_sending_without_reply': None,
+                                'reply_markup': None,
+                                'timeout': None,
+                                'data': None,
+                                'message_thread_id': None,
+                                'has_spoiler': None,
+                                'thumb': None},
                       "twig": {'twig': None}}
         self.params = self.types[content_type]
 
@@ -340,20 +409,20 @@ class Leaf:
     def check_condition(self, arg):
         return self.condition(arg)
 
-    def set_content_type(self, content_type):
+    def set_content_type(self, content_type): #
         if content_type not in ['text', 'photo', 'document']:
             raise TypeError({'Error': {'Type': type(content_type)}, 'attribute': {'Name': 'condition', 'Types': [type('')]}})
         self.content_type = content_type
         self.params = self.types[content_type]
         return True
 
-    def set_condition(self, condition):
+    def set_condition(self, condition): #
         if type(condition) != type(lambda: True):
             raise TypeError({'Error': {'Type': type(condition)}, 'attribute': {'Name': 'condition', 'Types': [type(lambda x: True)]}})
         self.condition = condition
         return True
 
-    def set_keyboard(self, keyboard):
+    def set_keyboard(self, keyboard): #
         if self.content_type == 'twig':
             raise TypeError({'Error': {'Type': 'twig'}, 'attribute': {'Name': 'keyboard', 'Types': ["'text'", "'photo'", "'document'"]}})
         if not ((keyboard != None) == (isinstance(keyboard, InlineKeyboardMarkup) or isinstance(keyboard, ReplyKeyboardMarkup))):
